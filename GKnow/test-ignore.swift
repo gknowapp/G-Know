67a1705980886693b23099d2bf573ceeb6b6f23a
Catherine Chu <105084561+ahhchu@users.noreply.github.com>
2024-09-19 12:57:26 -0400

    Create test-ignore.swift
    ignore - test pushing new file into repo

---
+++ GKnow/test-ignore.swift
@@ -0,0 +1,20 @@
+//
+//  test-ignore.swift
+//  GKnow
+//
+//  Created by Catherine Chu on 9/19/24.
+//
+
+import SwiftUI
+
+struct test_ignore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_ignore_Previews: PreviewProvider {
+    static var previews: some View {
+        test_ignore()
+    }
+}
